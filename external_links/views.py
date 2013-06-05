f1c375856306736eeb66c6e2adc835fc5a0e34d2
holger <holger@version-2.net>
2013-06-05 02:03:11 +0200

    [ADD] added default option to redirevt via JS Template having a ga templatetag

--- external_links/views.py
+++ external_links/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.http import HttpResponseRedirect
-
+from django.shortcuts import render_to_response
 from external_links.models import LinkClick
 
-def external_link(request):
+def external_link(request, use_ga_js = True):
     """
     Redirects links and keeps track of them
     """
@@ -16,5 +16,7 @@ def external_link(request):
         # Someone got here without the link param
         # Redirect to Home as default
         link = '/'
-
-    return HttpResponseRedirect(link)
+    if use_ga_js:
+        return render_to_response('external_links/ga_redirect.html', {'link': link})
+    else:
+        return HttpResponseRedirect(link)
